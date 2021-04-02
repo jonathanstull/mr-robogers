e0e57d6e9442023f58b9e196f9b506cf31a8a58c
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-02 14:04:57 -0700

    passes test 6 and new test 7 that returns an array counting down from any integer

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,55 @@ function countDown(fromNumber) {
   let array = [];
   array.unshift(fromNumber);
   let nextNumber = fromNumber - 1;
-  if (nextNumber > 0) {
-    countDown(nextNumber);
+  while (nextNumber > 0) {
+    array.unshift(nextNumber);
+    nextNumber = nextNumber - 1;
   };
   return array;
 };
 
 // Business logic
 
-function robogerize(input) {
-  let output = [];
-  if (isNaN(input[0])) {
+function robogerize(int) {
+  let input = parseInt(int);
+  let outputArray = [];
+  if (isNaN(input)) {
     return "Beep boop, not a number, neighbor. Please try again.";
-  } else if (input[0] < 0) {
-    return output;
-  } else if (input[0] >= 0) {
-    output = new Array(input[0]);
-    for (i = input[0]; i <= input[0]; i--) {
-      output.unshift(input[0]);
-      input[0] = [input[0] - 1];
-      robogerize(input[0]);
+  } else if (input === 0) {
+    outputArray.unshift(input);
+    return outputArray;
+  } else if (input > 0) {
+    outputArray.unshift(input);
+    let newInput = input - 1;
+    while (newInput >= 0) {
+      outputArray.unshift(newInput)
+      newInput = newInput - 1;
     };
   } else {
     return "I am a banana";
   };
+  return outputArray;
 };
+
+
+
+
+// function robogerize(input) {
+//   let output = [];
+//   if (isNaN(input[0])) {
+//     return "Beep boop, not a number, neighbor. Please try again.";
+//   } else if (input[0] < 0) {
+//     return output;
+//   } else if (input[0] >= 0) {
+//     output = new Array(input[0]);
+//     for (i = input[0]; i <= input[0]; i--) {
+//       output.unshift(input[0]);
+//       input[0] = [input[0] - 1];
+//       robogerize(input[0]);
+//     };
+//   } else {
+//     return "I am a banana";
+//   };
+// };
 
 // User interface logic
