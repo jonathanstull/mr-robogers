796ef7f9e2a7a1a8e027fe7ae36affe14f6d066d
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-02 11:06:27 -0700

    it returns an array [0, 1]; declare test 5 when input is NaN

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Utility logic
 
-const input = [0];
+let input = [1];
 
 // Business logic
 
@@ -12,6 +12,12 @@ function robogerize() {
   } else if (input[0] === 0) {
     output.push(input[0]);
     return output;
+  } else if (input[0] >= 1) {
+    output.push(input[0]);
+    for (i = 0; i < input.length; i += 1) {
+      output.push(input[i] - 1);
+      return output.reverse();
+    };
   } else {
     return "I am a banana";
   };
